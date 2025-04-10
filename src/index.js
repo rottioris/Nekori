837abcc8434bde234b97f34f1c662a86d0753cb5
Rottioris <rottioris@gmail.com>
2025-04-09 23:06:39 -0500

    first commit / React

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as App } from './App';
+export * from './context/AuthContext';
+export * from './main';
+export * from './styles/breackpoints';
+export * from './styles/themes';
+export * from './styles/variables';
+export * from './supabase/supabase.config';
+export * from './utils/dataStatic';
